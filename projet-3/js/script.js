ce9b16894e973e39bea3e14ce3ea02b4d87b7c9d
AlexandreAydin <alexandreaydin1993@gmail.com>
2023-05-21 20:49:44 +0200

    🎉 project -3 finish

--- projet-3/js/script.js
+++ projet-3/js/script.js
@@ -1,24 +1,36 @@
 const arrows = document.querySelectorAll('.arrow');
 const movieLists = document.querySelectorAll('.movie-list');
 
-  
+
+
 arrows.forEach((arrow, i) => {
-    let clickCounter = 0;
-    const imageItem = movieLists[i].querySelectorAll('img').length;
-    const totalImages = 4; // Nombre d'images affichées à la fois
+  let clickCounter = 0;
+  const imageItem = movieLists[i].querySelectorAll('img').length;
   
-    arrow.addEventListener('click', function() {
-      clickCounter++;
-  
-      // Réinitialise le compteur de clics si toutes les images ont été défilées
-      if (clickCounter >= imageItem - totalImages + 1) {
-        clickCounter = 0;
-        movieLists[i].style.transform = 'translateX(0)';
-      } else {
-        movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get('transform')[0].x.value - 300}px)`;
-      }
-    });
+  arrow.addEventListener('click', function() {
+    const widthRatio = Math.floor(window.innerWidth / 300)
+    clickCounter++;
+    // Réinitialise le compteur de clics si toutes les images ont été défilées
+    if (imageItem - (4 + clickCounter) +(4 - widthRatio ) >=0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get('transform')[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)"
+      // clickCounter=0;
+    }
   });
+});
+
+//   // Dark mode start //
+const ball = document.querySelector('.toggle-ball');
+const items = document.querySelectorAll('.navbar,.sidebar,.sidebar i,.movie-list-filter,.movie-list-container,.toggle,.toggle-ball,.movie-list-filter select');
+
+ball.addEventListener("click", function(){
+  items.forEach((item) => item.classList.toggle('active'));
+});
+
+   // Dark mode end//
   
   
 
